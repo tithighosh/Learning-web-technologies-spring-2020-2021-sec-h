a6b7786f0a70bebfc03a4ce06ae01cb36e786d94
Tithi <78010125+tithighosh@users.noreply.github.com>
2021-02-16 19:11:08 +0600

    Update Q8.php

--- MID_LAB_TASK_PHP_04/Q8.php
+++ MID_LAB_TASK_PHP_04/Q8.php
@@ -1,16 +1,49 @@
-<?php 
+<b>Question 8<br></b>
+<?php
+	echo "The array to declare";
+	echo "<br>";
 
-$myarray = array( 
-      
+	$array=[
+		[1,2,3,'A'],
+		[1,2,'B','C'],
+		[1,'D','E','F']
+	];
 
-    array("1", "2", "3","A","<br />"), 
-    array("1", "2", "B","C","<br />") ,
-    array("1", "D", "E","F") ,
-    
-    
+	echo $array[0][0]." ".$array[0][1]." ".$array[0][2]." ".$array[0][3];
+	echo "<br>";
+	echo $array[1][0]." ".$array[1][1]." ".$array[1][2]." ".$array[1][3];
+	echo "<br>";
+	echo $array[2][0]." ".$array[2][1]." ".$array[2][2]." ".$array[2][3];
 
-); 
-      
+	echo "<br>";
+	echo "Sheaps to print";
+	echo "<br>";
 
- print_r($myarray); 
-?> 
+	$array=[
+		[1,2,3],
+		[1,2],
+		[1]
+	];
+
+	echo $array[0][0]." ".$array[0][1]." ".$array[0][2];
+	echo "<br>";
+	echo $array[1][0]." ".$array[1][1];
+	echo "<br>";
+	echo $array[2][0];
+
+	echo "<br>";
+	echo "&";
+	echo "<br>";
+
+	$array=[
+		['A'],
+		['B','C',],
+		['D','E','F']
+	];
+
+	echo $array[0][0];
+	echo "<br>";
+	echo $array[1][0]." ".$array[1][1];
+	echo "<br>";
+	echo$array[2][0]." ".$array[2][1]." ".$array[2][2];
+?>

8a65883b73d8a0b7661c38d1d65a302e0afe0811
Tithi <78010125+tithighosh@users.noreply.github.com>
2021-03-01 23:32:57 +0600

    Forgot Password

---
+++ MID_LAB_TASK_PHP_SESSION_07/D_Forgot_Password.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+     
+    <title>forgot password</title>
+</head>
+<body>
+    <fieldset>
+        <table width='100%' >
+            <tr>
+            <td>
+                    <img src="logo.png" alt="Italian Trulli" height='60px'>
+                 
+                </td> 
+                <td align='right'>
+                    <nav>
+                        <a href='./A_Public_Home.php'>Home</a> |
+                        <a href='./C_Login.php'>Log In</a> |
+                        <a href='./B_Registration.php'>Registration</a>
+                    </nav>
+                </td>
+            </tr>
+        </table>
+    </fieldset>
+    <fieldset>
+        <br> <br><br>
+        <label>
+            
+            <b>
+        <form method="post" action="">
+      <fieldset>
+        <legend><b>FORGOT PASSWORD</b></legend>
+        <table>
+                      
+            
+            <tr>
+              <td>Email</td>
+              <td>:<input type="email" name="email" value=""></td>
+            </tr>
+            <tr>
+              
+        </table>
+      <hr style="width:25%;text-align:left;margin-left:0">
+         <input type="submit" name="submit" value="Submit"> 
+        
+      </fieldset>
+    </form>
+                </b>
+           
+        </label>
+        <br><br><br><br>
+    </fieldset>
+    <fieldset >
+        <center>
+            <label>
+            <b>
+                Copyright © 2017
+                </b>
+            </label>
+        </center>
+    </fieldset>
+</body>
+</html>

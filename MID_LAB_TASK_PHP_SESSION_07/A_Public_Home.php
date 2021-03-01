f041d39a40aca046a4e9eed9d58b85aaf5406f77
Tithi <78010125+tithighosh@users.noreply.github.com>
2021-03-01 23:31:20 +0600

    Public Home

---
+++ MID_LAB_TASK_PHP_SESSION_07/A_Public_Home.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+     
+    <title>Public Home</title>
+</head>
+<body>
+    <fieldset>
+        <table width='100%' >
+            <tr>
+            <td>
+                    <img src="logo.png" alt="Italian Trulli" height='60px'>
+                 
+                </td> 
+                <td align='right'>
+                    <nav>
+                        <a href='./A_Public_Home.php'>Home</a> |
+                        <a href='./C_Login.php'>Log In</a> |
+                        <a href='./B_Registration.php'>Registration</a>
+                    </nav>
+                </td>
+            </tr>
+        </table>
+    </fieldset>
+    <fieldset>
+        <br> <br><br>
+        <label>
+            <h1>
+            <b>
+                Welcome to X Company
+                </b>
+            </h1>
+        </label>
+        <br><br><br><br>
+    </fieldset>
+    <fieldset>
+        <center>
+            <label>
+            <b>
+                Copyright © 2017
+                </b>
+            </label>
+        </center>
+    </fieldset>
+</body>
+</html>

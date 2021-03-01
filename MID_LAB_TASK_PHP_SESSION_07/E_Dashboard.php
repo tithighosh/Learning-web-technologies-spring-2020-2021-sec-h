b7595488d32d65d20678e45e251985b783c532e3
Tithi <78010125+tithighosh@users.noreply.github.com>
2021-03-01 23:33:27 +0600

    Dashboard

---
+++ MID_LAB_TASK_PHP_SESSION_07/E_Dashboard.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+     
+    <title>Dashboard</title>
+</head>
+<body>
+    <fieldset>
+        <table border="0" align="center" width="100%">
+            <tr>
+            <td>
+                    <img src="logo.png" alt="Italian Trulli" height='60px'>
+                 
+                </td> 
+                <td align='right'>
+                    <nav>
+                        Logged in as<a href='./'> Bob</a> |
+                        <a href='./A_Public_Home.php'>Logout</a> 
+                       
+                    </nav>
+                </td>
+            </tr>
+        </table>
+    </fieldset>
+    <fieldset>
+        <br> <br><br>
+        <label>
+            <table border="0" align="center" width="100%">
+            <tr width="500px" height="100px">
+            <td >
+            <h3> Account </h3>
+            <hr style="width:50%;text-align:left;margin-left:0">
+            
+              <li> <a href='./E_Dashboard.php'>Dashboard   
+                </a> <br> </li>
+               <li><a href='./F_View_Profile'>View Profile</a> <br></li>
+              <li> <a href='./G_Edit_Profile.php'>Edit Profile</a><br></li>
+               <li><a href='./H_Profile_Picture.php'>Change Profile Picture</a><br></li>
+              <li> <a href='./I_Change_Password.php'>Change Password</a><br></li>
+               <li><a href='./A_Public_Home.php'>Logout</a><br></li>
+            
+            </td>
+            <td><h2>Wlocome Bob</h2></td>
+            </tr>
+            </table> 
+           
+
+        <div class="vl"></div>  
+           
+        </label>
+        <br><br><br><br>
+    </fieldset>
+    <fieldset>
+        <center>
+            <label>
+            <b>
+
+                Copyright © 2017
+                </b>
+            </label>
+        </center>
+    </fieldset>
+</body>
+</html>

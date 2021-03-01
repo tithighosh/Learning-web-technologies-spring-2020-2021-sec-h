66d17fab632aef19245ec7dab34b5220a7f7fa86
Tithi <78010125+tithighosh@users.noreply.github.com>
2021-03-01 23:32:29 +0600

    Login

---
+++ MID_LAB_TASK_PHP_SESSION_07/C_Login.php
@@ -0,0 +1,118 @@
+<?php
+
+    session_start();
+    
+    if(isset($_POST['submit']))
+    {
+        
+        
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if ($username == "" || $password == "")
+        {
+            echo "Null Submission";
+        }
+        else
+        {
+            if (strlen($username)<3) 
+                {
+                    echo "UserName must contain at least two (2) characters";           
+                }
+            elseif (strlen($password)<8)
+
+                {
+
+                    echo "Password must not be less than eight(8) characters";
+                }
+            
+
+            else
+            {
+                    if (ord($username >= 97 && ord($username) <= 122) || (ord($username) >= 65 && ord($username) <= 90) || (ord($username) >= 48 && ord($username) <= 57) || ord($username == 39) || ord($username == 45) || ord($username == 95)) 
+
+                    {
+                        echo "<i>Submission Succefull</i></b><br>";
+                        header('location: Login.html');
+                    }
+
+                    else
+                    {
+                        echo " User Name can contain alpha numeric characters, period,dash or underscore only";
+                    }
+            }
+                
+        }
+    }
+?>
+
+<html>
+<head>
+     
+    <title>Login</title>
+</head>
+<body>
+    <fieldset>
+        <table width='100%' >
+            <tr>
+            <td>
+                    <img src="logo.png" alt="Italian Trulli" height='60px'>
+                 
+                </td> 
+                <td align='right'>
+                    <nav>
+                        <a href='./A_Public_Home.php'>Home</a> |
+                        <a href='./C_Login.php'>Log In</a> |
+                        <a href='./B_Registration.php'>Registration</a>
+                    </nav>
+                </td>
+            </tr>
+        </table>
+    </fieldset>
+    <fieldset>
+        <br> <br><br>
+        <label>
+            
+           
+                <form method="post" action="Login.php">
+      <fieldset>
+        <legend><b><h1>LOGIN</h1></b></legend>
+        <table>
+          <tr>
+            <td>username:</td>
+            <td><input type="text" name="username" value=""></td>
+          </tr>
+          <tr>
+            <td>password:</td>
+            <td><input type="password" name="password" value=""></td>
+          </tr>
+
+          <br>
+         
+
+        </table>
+      <hr style="width:25%;text-align:left;margin-left:0">
+            <input type="checkbox" name="checkbox" value="checkbox" >Remember Me
+        <br>
+        <br>
+        <input type="submit" name="submit" value="Submit"> 
+        <a href='./D_Forgot_Password.php'><u>Forgot Password?</u></a>
+        
+      </fieldset>
+    </form>
+         
+        </label>
+        <br><br><br><br>
+    </fieldset>
+    <fieldset>
+        <center>
+            <label>
+            <b>
+                Copyright © 2017
+                </b>
+            </label>
+        </center>
+    </fieldset>
+</body>
+</html>
